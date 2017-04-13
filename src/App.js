ca03e48c3f16c75a6fa1eb47eed780c4d27db588
gnkatchmar <gunk55@msn.com>
2017-04-13 16:21:38 -0700

    Update Home, move NavBar into App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import NavTabs from './NavTabs';
+import React from "react";
+import ReactDOM from "react-dom";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from "react-router-dom"
+import { Tabs, Tab } from "material-ui-scrollable-tabs/Tabs";
+import Portfolio from "./Portfolio";
+import Home from "./Home";
+import Dining from "./Dining";
+import Cocktail from "./Cocktail"
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
 const App = () => (
+    /*<Router>
+    <Route exact path="/" component={Home}/>
+    <Route path="/portfolio" component={Portfolio}/>
+    <Route path="/dining" component={Dining}/>
+    <Route path="/Cocktail" component={Cocktail}/>
+  </Router>*/
   <MuiThemeProvider>
     <div>
-      <NavTabs />
+       <Tabs tabType="scrollable-buttons">
+          <Tab label="Home" >
+            <div>
+              <h2 style={styles.headline}>Gregory N. Katchmar</h2>
+              <h3>Full-stack JavaScript Developer</h3>
+              <h4>HTML, CSS, JavaScript, Node.js, React.js, Angular.js 1.5, MongoDB, Express.js, SQL, webpack, testing (Mocha, Chai, Karma, Protrator, Jest)</h4>
+              <Home />
+            </div>
+          </Tab>
+          <Tab label="Portfolio" >
+            <div>
+              <h2 style={styles.headline}>Projects</h2>
+              <Portfolio />
+            </div>
+          </Tab>
+          <Tab label="Dining Guide" >
+            <div>
+              <Dining />
+            </div>
+          </Tab>
+          <Tab label="Cocktail Guides" >
+            <div>
+              <Cocktail />
+            </div>
+          </Tab>
+      </Tabs>
     </div>
   </MuiThemeProvider>
 );
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 export default App;
