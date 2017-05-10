e4565799d861f27f60dfb23da166dc27e6f1bb34
gnkatchmar <gunk55@msn.com>
2017-05-10 15:13:53 -0700

    restaurant rating updates

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -14,13 +14,13 @@ const Restaurants = () => (
 
       <p><b>4 stars</b>: Pix/Bar Vivant&nbsp;(twice), Foster Burger, Stammtisch, Imperial, Cibo&nbsp;(seven visits), Chiang Mai, Ha &&nbsp;Vl (twice), Riyadh's (only have had home delivery), Tacos Chavez Express 2 (only have had home delivery), Pok Pok, <del>Vincentes</del>, Accanto, Redwood, Old Salt Marketplace, Kachka*, Tamale Boy&nbsp;(twice), Lardo&nbsp;(twice), Taqueria Portland (twice), Sayler's Old Country Kitchen&nbsp;(four times), Church, Red Star Tavern, Karam, <del>Skyline Burgers&nbsp;(Broadway)</del>, Grain and Gristle&nbsp;(twice), Pho Corner, Chez Machin&nbsp;(twice), Xico, <del>2nd Story&nbsp;(twice)</del>, Woodsman Tavern, <del>Sen Yai</del>, <del>Pho Van&nbsp;(Hawthorne)</del>, Oaks Bottom Public&nbsp;House, Gyro House Mediterranean Grill (Beaverton)</p>
 
-      <p><b>3 ½ stars</b>: Gold Dust Meridian&nbsp;(twice), <del>Iorio&nbsp;(twice)</del>, Picnic House, Hoda’s Middle-Eastern Cuisine, Bit House Saloon, Biwa Izakaya*, Juan Colorado, <del>Mike's Drive-In&nbsp;(twice)</del>, La Sirenita, <del>Bywater Grocery</del>, Ringside Fish House, <del>Bar Dobre</del>, Kenny &&nbsp;Zuke’s Delicatessen, Taqueria Los Gorditos, La Fondita&nbsp;(Salem), The Goose, Hale Pele, Grassa, DeNicola's (twice), Sapphire Hotel, Teote, Roost, <del>Lucky Strike&nbsp;(thrice)</del>, Nicholas II*, <del>Oso</del>, Tapalaya*, <del>Tabla</del>, Olympic Provisions&nbsp;(Eastside), El Cubo de Cuba&nbsp;(four times), Bazi Bierbrasserie&nbsp;(twice), El Sombrero Tapatio, Uno Mas, <del>Club&nbsp;21</del>, Two Brothers Café &&nbsp;Grill&nbsp;(twice)</p>
+      <p><b>3 ½ stars</b>: Gold Dust Meridian&nbsp;(twice), <del>Iorio&nbsp;(twice)</del>, Picnic House, Hoda’s Middle-Eastern Cuisine, Bit House Saloon, Biwa Izakaya*, Juan Colorado, <del>Mike's Drive-In&nbsp;(twice)</del>, La Sirenita, <del>Bywater Grocery</del>, Ringside Fish House, <del>Bar Dobre</del>, Kenny &&nbsp;Zuke’s Delicatessen, Taqueria Los Gorditos, La Fondita&nbsp;(Salem), The Goose, Hale Pele, Grassa, DeNicola's (twice), Sapphire Hotel, Teote, Roost, <del>Lucky Strike&nbsp;(thrice)</del>, Nicholas II*, <del>Oso</del>, Tapalaya*, <del>Tabla</del>, Olympic Provisions&nbsp;(Eastside), El Cubo de Cuba&nbsp;(four times), Bazi Bierbrasserie&nbsp;(twice), El Sombrero Tapatio, Uno Mas, <del>Club&nbsp;21</del>, Two Brothers Café &&nbsp;Grill&nbsp;(twice), Five Guys</p>
 
-      <p><b>3 stars</b>: Pollo Norte 2.0 (twice), La Panza, Mama Leo’s, St. Jack, Noraneko (twice), Yen Ha, The Lamp&nbsp;(four times), Hot Lips Pizza&nbsp;(twice), Sizzle Pie&nbsp;(twice), Tambayan, Fujiyama Sushi and Grill, Dove Vivi, Fogo de Chao, Salvador Molly’s, Double Dragon&nbsp;(twice), Hop City Tavern, The Ranger Station, Dan and Louis Oyster Bar, Hawthorne Hophouse&nbsp;(twice), <del>Branch Whiskey Bar</del>, <del>SubRosa</del>, <del>Kenny &&nbsp;Zuke’s Deli Bar</del>, The Brooklyn House, Laurelwood Brewpub, Hopworks Urban Brewery, Slowburger&nbsp;(twice), Delta Café&nbsp;(thrice), Knuckle Sandwich&nbsp;(twice), Double&nbsp;Barrel, Migration Brewing (twice), Nick's Famous Coney Island (twice), Fresh Roll (thrice), Blackbird Pizza (twice)</p>
+      <p><b>3 stars</b>: Pollo Norte 2.0 (twice), La Panza, Mama Leo’s, St. Jack, Noraneko (twice), Yen Ha, The Lamp&nbsp;(four times), Hot Lips Pizza&nbsp;(twice), Sizzle Pie&nbsp;(twice), Tambayan, Fujiyama Sushi and Grill, Dove Vivi, Fogo de Chao, Salvador Molly’s, Double Dragon&nbsp;(twice), Hop City Tavern, The Ranger Station, Dan and Louis Oyster Bar, Hawthorne Hophouse&nbsp;(twice), <del>Branch Whiskey Bar</del>, <del>SubRosa</del>, <del>Kenny &&nbsp;Zuke’s Deli Bar</del>, The Brooklyn House, Laurelwood Brewpub, Hopworks Urban Brewery, Slowburger&nbsp;(twice), Delta Café&nbsp;(thrice), Knuckle Sandwich&nbsp;(twice), Double&nbsp;Barrel, Migration Brewing (twice), Nick's Famous Coney Island (twice), Fresh Roll (thrice), Blackbird Pizza (twice), Smokehouse Tavern (twice)</p>
 
     <h3>Breakfast/Brunch (in order)</h3>
 
-      <p><del>Simpatica&nbsp;(thrice)*</del>, Screen Door, Country Cat&nbsp;(four visits), Beach Dog Cafe&nbsp;(Lincoln City), Pepper Box&nbsp;(twice), Pine State Biscuits&nbsp;(six visits), Petite Provence&nbsp;(thrice), Muscadine, Redwood&nbsp;(twice), Zell’s&nbsp;(thrice), St. Honoré Bakery, Midpoint Café&nbsp;(twice), Doug Fir&nbsp;(seven visits), Jam&nbsp;(five visits), Block House Cafe (Dayton), Ya Hala (thrice), Cheryl's on 12th, Cadillac Café&nbsp;(nine visits), The Waffle Window&nbsp;(thrice), Off the Waffle, Utopia Café&nbsp;(thrice), <del>Kerns Kitchen&nbsp;(twice)</del>, Bridges Café&nbsp;(twice), <del>Blue Pig Café</del>, Sanborn's, Hazel Room&nbsp;(twice), Chez Machin&nbsp;(twice), Swift Lounge&nbsp;(no longer serve brunch), City State Diner, Slappy Cakes&nbsp;(thrice), Sckavone’s (twice), Tabor Bread, New Deal Cafe, Brick and Mortar&nbsp;(Albany), Tom's Restaurant (twice), Cup and Saucer (twice), Bottle +&nbsp;Kitchen&nbsp;(thrice), Le Bistro Montage&nbsp;(twice), Firehouse Grill&nbsp;(Seaside), Cameo Café, Junior's Cafe, Milo's City Cafe (twice), Cricket Café&nbsp;(twice), EastBurn, Holman's, Hawthorne Hophouse</p>
+      <p><del>Simpatica&nbsp;(thrice)*</del>, Screen Door, Country Cat&nbsp;(four visits), Beach Dog Cafe&nbsp;(Lincoln City), Pepper Box&nbsp;(twice), Pine State Biscuits&nbsp;(six visits), Petite Provence&nbsp;(thrice), Muscadine, Redwood&nbsp;(twice), Zell’s&nbsp;(thrice), St. Honoré Bakery, Midpoint Café&nbsp;(twice), Doug Fir&nbsp;(seven visits), Jam&nbsp;(five visits), Block House Cafe (Dayton), Ya Hala (thrice), Cheryl's on 12th, Cadillac Café&nbsp;(nine visits), The Waffle Window&nbsp;(thrice), Off the Waffle, Utopia Café&nbsp;(thrice), <del>Kerns Kitchen&nbsp;(twice)</del>, Bridges Café&nbsp;(twice), <del>Blue Pig Café</del>, Sanborn's, Hazel Room&nbsp;(twice), Chez Machin&nbsp;(twice), Swift Lounge&nbsp;(no longer serve brunch), City State Diner, Slappy Cakes&nbsp;(thrice), Sckavone’s (twice), Tabor Bread, New Deal Cafe, Brick and Mortar&nbsp;(Albany), Tom's Restaurant (twice), Cup and Saucer (twice), Bottle +&nbsp;Kitchen&nbsp;(thrice), Le Bistro Montage&nbsp;(twice), Firehouse Grill&nbsp;(Seaside), Cameo Café, Junior's Cafe, Milo's City Cafe (twice)</p>
 
     <h3>Favorite Food Carts&nbsp;(alpha order, based on multiple visits)</h3>
 
@@ -31,22 +31,17 @@ const Restaurants = () => (
       <p>East Side Delicatessen&nbsp;(Hawthorne)&nbsp;(dozens of visits; well-seasoned, large sandwiches), Franks a Lot&nbsp;(dozens of visits; drive-through hot dogs &&nbsp;sausages)</p>
 
     <h3>Dining Fouls</h3>
-
+    
       <p>Cafe Broder*&nbsp;(loud, long wait for overpriced ($50 for two adults and one child) breakfast)</p>
-
+      <p>Cricket Cafe&nbsp;(burnt one side of pancakes and served burnt side down instead of redoing)</p>
       <p>EastBurn&nbsp;(large amount of unwanted onions and garlic not
       mentioned in menu item descriptions)</p>
-
       <p><del>Fressen Artisan Bakery</del>&nbsp;(unapologetically bad service including a 40-minute wait for trivial amount of spaetzle)</p>
       <p>Frank's Noodle House (loud and one-dimensionally seasoned dishes)</p>
-
       <p><del>Lotus Cardroom and Café</del>&nbsp;(inedible salty demi-glace on dinner
       entrée)</p>
-
       <p>Portland Prime&nbsp;(sad attempt at a steak house; supermarket-like cheese plate)</p>
-
       <p>Tabor Tavern (after long wait, waffle and $13 burger both arrived overdone/charred)</p>
-
       <p><del>Trigger*</del>&nbsp;(DEAFENING, overpriced limited menu,
       wouldn’t modify desired item)</p>
 
