1b93858af4e797ddef81236db5531c77c069296f
gnkatchmar <gunk55@msn.com>
2017-07-26 08:21:24 -0700

    restaurant updates

--- src/Home.js
+++ src/Home.js
@@ -32,7 +32,7 @@ const Home = () => (
     <h4>Contact me at:</h4>
     <a href="mailto:gregkatchmar@gmail.com">gregkatchmar@gmail.com</a>
     <hr></hr>
-    <p>Last updated July 7, 2017</p>
+    <p>Last updated July 26, 2017</p>
   </div>
 );
 
