05056d99ade4e113419db11c4897fd1157774d81
gnkatchmar <gunk55@msn.com>
2017-09-06 08:20:27 -0700

    Update date change for build

--- src/Home.js
+++ src/Home.js
@@ -40,7 +40,7 @@ class Home extends Component {
         <h4>Contact me at:</h4>
         <a href="mailto:gregkatchmar@gmail.com">gregkatchmar@gmail.com</a>
         <hr></hr>
-        <p>Last updated August 26, 2017</p>
+        <p>Last updated September 6, 2017</p>
         <hr></hr>
         <p>A React/material-ui site</p>
       </div>
