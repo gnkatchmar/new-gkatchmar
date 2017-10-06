4cb6668dead8570dbf5e49831ec0dc1ca024dda7
gnkatchmar <gunk55@msn.com>
2017-10-06 15:42:22 -0700

    Portfolio works

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,113 +1,222 @@
-import React, {Component} from "react";
-import {Card, CardHeader, CardMedia, CardTitle, CardText} from "material-ui/Card";
-import injectTapEventPlugin from "react-tap-event-plugin";
+// import React, {Component} from "react";
+// import {Card, CardHeader, CardMedia, CardTitle, CardText} from "material-ui/Card";
+// import injectTapEventPlugin from "react-tap-event-plugin";
+
+// const cardData = [{
+//   "key": 1,
+// 	"headerTitle": "Portland Weekend Brunch Finder 2.0",
+// 	"headerSubtitle": "September 2017",
+//   "headerAvatar": require("./avatars/201FinalAvatar.jpg"),
+//   "expandable": true,
+//   "media": require("./avatars/Brunch.jpg"),
+// 	"titleTitle": 'Streamlined, mobile-responsive version of Code Fellows 201 Final Project. 200+ restaurants from all five "Portland Quadrants."',
+// 	"titleSubtitle": "HTML/CSS/JavaScript.",
+// 	"textHref": "https://github.com/gnkatchmar/brunch-finder"
+// },
+// {
+//   "key": 2,
+// 	"headerTitle": "gkatchmar.com",
+// 	"headerSubtitle": "May 2017",
+//   "headerAvatar": require("./avatars/GK.png"),
+//   "expandable": false,
+//   "media": "",
+// 	"titleTitle": "Personal website",
+// 	"titleSubtitle": "HTML/CSS/JavaScript + React, Material-UI, History, React-Scroll-Up.",
+// 	"textHref": "https://github.com/gnkatchmar/new-gkatchmar"
+// },
+// {
+//   "key": 3,
+// 	"headerTitle": "The BigToDo",
+// 	"headerSubtitle": "January 2017",
+//   "headerAvatar": require("./avatars/401FinalAvatar.jpg"),
+//   "expandable": true,
+//   "media": require("./avatars/401Final.jpg"),
+// 	"titleTitle": "Code Fellows 401 final team project using a full MEAN stack to create a streamlined, universal “to do” app.",
+// 	"titleSubtitle": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Angular 1.5, Node, Angular-Material, Angular-UI-Router, Webpack, Mocha/Chai/Karma/Jasmine.",
+// 	"textHref": "https://github.com/401-Final"
+// },
+// {
+//   "key": 4,
+// 	"headerTitle": "Spotify Fan Club",
+// 	"headerSubtitle": "November 2016",
+//   "headerAvatar": require("./avatars/401MidTermAvatar.jpg"),
+//   "expandable": true,
+//   "media": require("./avatars/401MidTerm.jpg"),
+// 	"titleTitle": "Code Fellows 401 midterm team project focusing on the backend and using the Spotify API with OAuth to create a social app for Spotify users (incomplete).",
+// 	"titleSubtitle": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Node, Mocha/Chai, Bootstrap, Handlebars, jQuery.",
+// 	"textHref": "https://github.com/pdx-fanclub/fanclub-app"
+// },
+// {
+//   "key": 5,
+// 	"headerTitle": "Food Truck Finder",
+// 	"headerSubtitle": "October 2016",
+//   "headerAvatar": require("./avatars/301FinalAvatar.jpg"),
+//   "expandable": true,
+//   "media": require("./avatars/301Final.jpg"),
+// 	"titleTitle": "Code Fellows 301 final team project using Yelp API, geolocation, and MVC design to create a mobile-first, responsive app that streamlines the food truck finding process (incomplete).",
+// 	"titleSubtitle": "HTML/CSS/JavaScript + Express, Skeleton.css, Handlebars, jQuery.",
+// 	"textHref": "https://github.com/foodtruck-finder/app"
+// },
+// {
+//   "key": 6,
+// 	"headerTitle": "Portland Weekend Brunch Finder",
+// 	"headerSubtitle": "August 2016",
+//   "headerAvatar": require("./avatars/201FinalAvatar.jpg"),
+//   "expandable": true,
+//   "media": require("./avatars/201Final.jpg"),
+// 	"titleTitle": "Code Fellows 201 final team project using geolocation, Google Places and Maps APIs to create a brunch finding app.",
+// 	"titleSubtitle": "HTML/CSS/JavaScript.",
+// 	"textHref": "https://github.com/gnkatchmar/Code201Final"
+// }
+// ]
+
+// // Needed for onTouchTap
+// injectTapEventPlugin();
+
+// export default class Portfolio extends Component {
+
+//   render(){
+//     let cardList = []
+//     Object.keys(cardData).forEach((cardIndex) => {
+//       let card = cardData[cardIndex]
+//       cardList.push (
+//         <div>
+//           <Card key={card.key}>
+//             <CardHeader
+//               title={card.headerTitle}
+//               subtitle={card.headerSubtitle}
+//               avatar={card.headerAvatar}
+//               actAsExpander={true}
+//               showExpandableButton={true}
+//             />
+//             <CardMedia
+//               expandable={card.expandable}
+//           >
+//             <img src={card.media} alt=""/>
+//           </CardMedia>
+//             <CardTitle title={card.titleTitle} subtitle={card.titleSubtitle} expandable={true} />
+//             <CardText expandable={true}>
+//               <a href={card.textHref} target="_blank" rel="noopener noreferrer">Project Link</a>
+//             </CardText>
+//           </Card>
+//         </div>            
+//       )
+//     })
+
+//     return (
+//       <div className='card-list'>
+//         <h2>Projects</h2>
+//           { cardList }
+//       </div>
+//     );
+//   }
+// }
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+// import classnames from 'classnames';
+import Card, { CardHeader, CardMedia, CardContent } from 'material-ui/Card';
+// import Typography from 'material-ui/Typography';
 
 const cardData = [{
   "key": 1,
 	"headerTitle": "Portland Weekend Brunch Finder 2.0",
-	"headerSubtitle": "September 2017",
-  "headerAvatar": require("./avatars/201FinalAvatar.jpg"),
-  "expandable": true,
-  "media": require("./avatars/Brunch.jpg"),
-	"titleTitle": 'Streamlined, mobile-responsive version of Code Fellows 201 Final Project. 200+ restaurants from all five "Portland Quadrants."',
-	"titleSubtitle": "HTML/CSS/JavaScript.",
+	"headerSubheader": "September 2017",
+  "mediaImage": require("./avatars/Brunch.jpg"),
+	"mediaTitle": 'Streamlined, mobile-responsive version of Code Fellows 201 Final Project. 200+ restaurants from all five "Portland Quadrants."',
+	"mediaSubheader": "HTML/CSS/JavaScript.",
 	"textHref": "https://github.com/gnkatchmar/brunch-finder"
 },
 {
   "key": 2,
 	"headerTitle": "gkatchmar.com",
-	"headerSubtitle": "May 2017",
-  "headerAvatar": require("./avatars/GK.png"),
-  "expandable": false,
-  "media": "",
-	"titleTitle": "Personal website",
-	"titleSubtitle": "HTML/CSS/JavaScript + React, Material-UI, History, React-Scroll-Up.",
+	"headerSubheader": "May 2017",
+  "mediaImage": "",
+	"mediaTitle": "Personal website",
+	"mediaSubheader": "HTML/CSS/JavaScript + React, Material-UI, React-Scroll-Up.",
 	"textHref": "https://github.com/gnkatchmar/new-gkatchmar"
 },
 {
   "key": 3,
 	"headerTitle": "The BigToDo",
-	"headerSubtitle": "January 2017",
-  "headerAvatar": require("./avatars/401FinalAvatar.jpg"),
-  "expandable": true,
-  "media": require("./avatars/401Final.jpg"),
-	"titleTitle": "Code Fellows 401 final team project using a full MEAN stack to create a streamlined, universal “to do” app.",
-	"titleSubtitle": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Angular 1.5, Node, Angular-Material, Angular-UI-Router, Webpack, Mocha/Chai/Karma/Jasmine.",
+	"headerSubheader": "January 2017",
+  "mediaImage": require("./avatars/401Final.jpg"),
+	"mediaTitle": "Code Fellows 401 final team project using a full MEAN stack to create a streamlined, universal “to do” app.",
+	"mediaSubheader": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Angular 1.5, Node, Angular-Material, Angular-UI-Router, Webpack, Mocha/Chai/Karma/Jasmine.",
 	"textHref": "https://github.com/401-Final"
 },
 {
   "key": 4,
 	"headerTitle": "Spotify Fan Club",
-	"headerSubtitle": "November 2016",
-  "headerAvatar": require("./avatars/401MidTermAvatar.jpg"),
-  "expandable": true,
-  "media": require("./avatars/401MidTerm.jpg"),
-	"titleTitle": "Code Fellows 401 midterm team project focusing on the backend and using the Spotify API with OAuth to create a social app for Spotify users (incomplete).",
-	"titleSubtitle": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Node, Mocha/Chai, Bootstrap, Handlebars, jQuery.",
+	"headerSubheader": "November 2016",
+  "mediaImage": require("./avatars/401MidTerm.jpg"),
+	"mediaTitle": "Code Fellows 401 midterm team project focusing on the backend and using the Spotify API with OAuth to create a social app for Spotify users (incomplete).",
+	"mediaSubheader": "HTML/CSS/JavaScript + MongoDB/Mongoose, Express, Node, Mocha/Chai, Bootstrap, Handlebars, jQuery.",
 	"textHref": "https://github.com/pdx-fanclub/fanclub-app"
 },
 {
   "key": 5,
 	"headerTitle": "Food Truck Finder",
-	"headerSubtitle": "October 2016",
-  "headerAvatar": require("./avatars/301FinalAvatar.jpg"),
-  "expandable": true,
-  "media": require("./avatars/301Final.jpg"),
-	"titleTitle": "Code Fellows 301 final team project using Yelp API, geolocation, and MVC design to create a mobile-first, responsive app that streamlines the food truck finding process (incomplete).",
-	"titleSubtitle": "HTML/CSS/JavaScript + Express, Skeleton.css, Handlebars, jQuery.",
+	"headerSubheader": "October 2016",
+  "mediaImage": require("./avatars/301Final.jpg"),
+	"mediaTitle": "Code Fellows 301 final team project using Yelp API, geolocation, and MVC design to create a mobile-first, responsive app that streamlines the food truck finding process (incomplete).",
+	"mediaSubheader": "HTML/CSS/JavaScript + Express, Skeleton.css, Handlebars, jQuery.",
 	"textHref": "https://github.com/foodtruck-finder/app"
 },
 {
   "key": 6,
 	"headerTitle": "Portland Weekend Brunch Finder",
-	"headerSubtitle": "August 2016",
-  "headerAvatar": require("./avatars/201FinalAvatar.jpg"),
-  "expandable": true,
-  "media": require("./avatars/201Final.jpg"),
-	"titleTitle": "Code Fellows 201 final team project using geolocation, Google Places and Maps APIs to create a brunch finding app.",
-	"titleSubtitle": "HTML/CSS/JavaScript.",
+	"headerSubheader": "August 2016",
+  "mediaImage": require("./avatars/201Final.jpg"),
+	"mediaTitle": "Code Fellows 201 final team project using geolocation, Google Places and Maps APIs to create a brunch finding app.",
+	"mediaSubheader": "HTML/CSS/JavaScript.",
 	"textHref": "https://github.com/gnkatchmar/Code201Final"
 }
 ]
 
-// Needed for onTouchTap
-injectTapEventPlugin();
+const styles = theme => ({
+  media: {
+    height: 400,
+  },
+});
 
-export default class Portfolio extends Component {
+class Portfolio extends React.Component {
 
   render(){
+    const classes = this.props.classes;
     let cardList = []
     Object.keys(cardData).forEach((cardIndex) => {
       let card = cardData[cardIndex]
       cardList.push (
-        <div>
-          <Card key={card.key}>
+        <div key={card.key} >
+          <Card className={classes.card}>
             <CardHeader
               title={card.headerTitle}
-              subtitle={card.headerSubtitle}
-              avatar={card.headerAvatar}
-              actAsExpander={true}
-              showExpandableButton={true}
+              subheader={card.headerSubheader}
             />
-            <CardMedia
-              expandable={card.expandable}
-          >
-            <img src={card.media} alt=""/>
-          </CardMedia>
-            <CardTitle title={card.titleTitle} subtitle={card.titleSubtitle} expandable={true} />
-            <CardText expandable={true}>
-              <a href={card.textHref} target="_blank" rel="noopener noreferrer">Project Link</a>
-            </CardText>
+            <CardContent>
+                {card.mediaTitle}<br /><br />
+                {card.mediaSubheader}<br /><br />
+                <a href={card.textHref} target="_blank" rel="noopener noreferrer">Project Link</a>
+            </CardContent>
           </Card>
         </div>            
       )
     })
 
     return (
-      <div className='card-list'>
-        <h2>Projects</h2>
-          { cardList }
-      </div>
+            <div className='card-list'>
+              <h2>Projects</h2>
+                { cardList }
+                </div>
     );
   }
 }
+
+Portfolio.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Portfolio);
