ae7344807f0a9e0e41ad015c088834338f07332e
gnkatchmar <gunk55@msn.com>
2017-11-07 06:32:36 -0800

    Two new restaurants

--- src/Home.js
+++ src/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
         <h4>Contact me at:</h4>
         <a href="mailto:gregkatchmar@gmail.com">gregkatchmar@gmail.com</a>
         <hr></hr>
-        <p>Last updated October 19, 2017</p>
+        <p>Last updated November 7, 2017</p>
         <hr></hr>
         <p>A React/Material-UI site</p>
         </div>
