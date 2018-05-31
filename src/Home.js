2c0ecbf5ae4cd7b404abf365082900bdff32c5b1
gnkatchmar <gunk55@msn.com>
2018-05-31 16:49:38 -0700

    update

--- src/Home.js
+++ src/Home.js
@@ -52,7 +52,7 @@ class Home extends Component {
         <hr></hr>
         <p>A React/Material-UI site</p>
         <hr></hr>
-        <p>Last updated: April 23, 2018</p>
+        <p>Last updated: May 31, 2018</p>
         </div>
     );
   }
