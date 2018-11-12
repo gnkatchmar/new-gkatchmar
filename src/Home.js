0b9cc95a8d958d2448a24d1fccf21f427f36a206
gnkatchmar <gunk55@msn.com>
2018-11-12 13:43:47 -0800

    small update + .htaccess

--- src/Home.js
+++ src/Home.js
@@ -52,7 +52,7 @@ class Home extends Component {
         <hr></hr>
         <p>A React/Material-UI site</p>
         <hr></hr>
-        <p>Last updated: October 23, 2018</p>
+        <p>Last updated: November 12, 2018</p>
         </div>
     );
   }
